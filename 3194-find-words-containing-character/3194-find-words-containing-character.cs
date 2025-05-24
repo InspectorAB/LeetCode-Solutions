1b2ab5aade0fa284b6ad1f9eef4d0f8ad9001524
Aditya Bhaskar <66431775+InspectorAB@users.noreply.github.com>
2025-05-24 12:39:33 +0530

    Time: 0 ms (100%), Space: 53.7 MB (39.43%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.cs
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cs
@@ -1,17 +1,13 @@
 public class Solution {
     public IList<int> FindWordsContaining(string[] words, char x) {
-        var res = new List<int>();
-
-        for(int j = 0; j < words.Length; j++){
-            char[] c = words[j].ToCharArray();
-            for(int i = 0; i < c.Length; i++){
-                if(c[i] == x){
-                    res.Add(j);
-                    break;
-                }
+        List<int> result = new List<int>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                result.Add(i);
             }
         }
-            
-        return res;
+        return result.ToArray();
     }
 }
