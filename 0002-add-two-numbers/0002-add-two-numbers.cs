05e2de0851f2351964951c3c101415effca2cabe
Aditya Bhaskar <66431775+InspectorAB@users.noreply.github.com>
2025-05-25 11:21:26 +0530

    Time: 0 ms (100%), Space: 53.5 MB (79.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -10,11 +10,12 @@
  * }
  */
 public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null && carry == 0) return null;
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry = 0) {
+     
+        if (l1 == null && l2 == null && carry == 0) return null;
 
         int total = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
         carry = total / 10;
-        return new ListNode(total % 10,  AddTwoNumbers(l1?.next, l2?.next, carry));
+        return new ListNode(total % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
     }
 }
